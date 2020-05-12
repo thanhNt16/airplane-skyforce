f010aff5e8651b1d5ac1fa465d189e2a01472694
thanh <thanh.nt163727@sis.hust.edu.vn>
2020-05-12 20:51:57 +0700

    update Start screen

--- src/client/communicate/Client.java
+++ src/client/communicate/Client.java
@@ -6,40 +6,43 @@ import java.net.*;
 import client.communicate.handler.MainHandler;
 
 class Client {
-    public static void main(String args[]) throws Exception {
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        MainHandler handler = new MainHandler(clientSocket, IPAddress, 9876);
-        Listener listener = new Listener(clientSocket, handler);
-        Thread t = new Thread(listener);
-        t.start();
-        String message = "";
-        while (!message.equals("END")) {
-        	message = inFromUser.readLine();
-        	handler.sendMessage(message);
-        }
-        listener.terminate();
-        clientSocket.close();
-    }
+	public static void main(String args[]) throws Exception {
+		Start frame = new Start();
+		frame.setVisible(true);
+
+//		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+//		DatagramSocket clientSocket = new DatagramSocket();
+//		InetAddress IPAddress = InetAddress.getByName("localhost");
+//		MainHandler handler = new MainHandler(clientSocket, IPAddress, 9876);
+//		Listener listener = new Listener(clientSocket, handler);
+//		Thread t = new Thread(listener);
+//		t.start();
+//		String message = "";
+//		while (!message.equals("END")) {
+//			message = inFromUser.readLine();
+//			handler.sendMessage(message);
+//		}
+//		listener.terminate();
+//		clientSocket.close();
+	}
 }
 
 class Listener implements Runnable {
 	private DatagramSocket clientSocket;
 	private boolean terminated = false;
 	private MainHandler handler;
-	
+
 	public Listener(DatagramSocket clientSocket, MainHandler handler) {
 		this.clientSocket = clientSocket;
 		this.handler = handler;
 	}
-	
+
 	public void terminate() {
 		terminated = true;
 	}
 
 	@Override
-    public void run() {
+	public void run() {
 		while (!terminated) {
 			try {
 				byte[] receiveData = new byte[1024];
