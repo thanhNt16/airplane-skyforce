fd6780ac6b60d73ef6374b694c3228751f9a175c
duchm <duchm@cystack.net>
2020-05-14 12:43:50 +0700

    update

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,12 +1,19 @@
 package client;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 import client.handler.MainHandler;
 
 class Client {
 	public static MainHandler handler;
+	
+	public static void main(String args[]) throws Exception {
+        ScreenManager.getInstance().display();
+        connect("localhost", 9876);
+	}
 	
 	public static void connect(String host, int port) throws Exception {
 		DatagramSocket clientSocket = new DatagramSocket();
--- src/client/Main.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-class Main {
-	public static void main(String args[]) throws Exception {
-        ScreenManager.getInstance().display();
-        Client.connect("localhost", 9876);
-	}
-}
